8d416c8a36b5be873ff77640f492637103aadeab
chuchix <piraquiveguacgehat@gmail.com>
2022-04-19 20:11:32 -0500

    bit

--- view/index.php
+++ view/index.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width, initial-scale=1.0">
     <title>Pagina porno de monitas chinas sin censura uwu</title>
+    <link rel="stylesheet" href="../public/css/bootstrap/bootstrap.min.css">
 </head>
 <body>
     <header style="color: white; background-color: blueviolet;">ESTA PAGINA ES PA VER HIDROGENOS UwU</header>
-    <h1>El joven no le sabe al chispost</h1>
+    <a class="btn btn-primary">enter</a>
 </body>
 </html>
